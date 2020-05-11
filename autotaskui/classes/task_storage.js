4f5243ce71cc369cb385b19586a52c496cdcd697
realToadtoad <kingdomkeepersguy@gmail.com>
2020-05-10 23:09:36 -0700

    fixed json stringing and parsing

--- autotaskui/classes/task_storage.js
+++ autotaskui/classes/task_storage.js
@@ -35,11 +35,13 @@ const TASK_STORAGE = "@TaskStorage:";
 export class TaskStorage {
   async getAllTasks(date) { // date is a string, YYYY-MM-DD
     let temp = AsyncStorage.getItem(TASK_STORAGE + date);
-    return temp;
+    let tempJSON = JSON.parse(temp);
+    return tempJSON;
   }
 
   async getTask(date, name) {
-    let temp = await AsyncStorage.getItem(TASK_STORAGE + date);
+    let temp1 = await AsyncStorage.getItem(TASK_STORAGE + date);
+    let temp = JSON.parse(temp1);
     result = temp.tasks.filter(item => item.name.toLowerCase() == name.toLowerCase());
     return result;
   }
@@ -49,11 +51,12 @@ export class TaskStorage {
     if (temp == undefined || temp == null) {
       temp = {tasks: []};
     }
-    temp.tasks.push(task);
+    let taskStr = JSON.stringify(task);
+    temp.tasks.push(taskStr);
     await AsyncStorage.setItem(TASK_STORAGE + date, temp);
   }
 
   async forceSetAllTasks(date, tasksObj) {
-    await AsyncStorage.setItem(TASK_STORAGE + date, tasksObj);
+    await AsyncStorage.setItem(TASK_STORAGE + date, JSON.stringify(tasksObj));
   }
 }
